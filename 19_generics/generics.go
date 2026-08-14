97d931d450017d7d380043009d4dabf5d53e627b
Vatsal Rathi <ds099315@gmail.com>
2026-08-14 12:50:20 +0530

    generics and channels done

--- 19_generics/generics.go
+++ 19_generics/generics.go
@@ -2,23 +2,35 @@ package main
 
 import "fmt"
 
-func printSlice[T comparable, V string](items []T, name V) {
-	for _, item := range items {
-		fmt.Println(item, name)
-	}
-}
-
-// func printStringSlice(items []string) {
+// func printSlice[T comparable, V string](items []T, name V) {
 // 	for _, item := range items {
-// 		fmt.Println(item)
+// 		fmt.Println(item, name)
 // 	}
 // }
 
+func printSlice[T any](items []T) {
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+
+func printSlicepipe[T int | string | bool](items []T) {
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+
+func printStringSlice(items []string) {
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+
 //
 // LIFO
-// type stack[T any] struct {
-// 	elements []T
-// }
+type stack[T any] struct {
+	elements []T
+}
 
 func main() {
 	// myStack := stack[string]{
@@ -28,8 +40,12 @@ func main() {
 	// fmt.Println(myStack)
 
 	// nums := []int{1, 2, 3}
-	// names := []string{"golang", "typescript"}
-	vals := []bool{true, false, true}
+	names := []string{"golang", "typescript"}
+	names2 := []int{1, 2, 3}
+	// vals := []bool{true, false, true}
 	// printStringSlice(names)
-	printSlice(vals, "john")
+	// printSlice(vals, "john")
+
+	printSlice(names)
+	printSlice(names2)
 }
--- 20_goroutines/goroutine.go
+++ 20_goroutines/goroutine.go
@@ -5,6 +5,11 @@ import (
 	"sync"
 )
 
+// func task(id int, w *sync.WaitGroup) {
+// 	defer w.Done()
+// 	fmt.Println("doing task", id)
+// }
+
 func task(id int, w *sync.WaitGroup) {
 	defer w.Done()
 	fmt.Println("doing task", id)
@@ -13,10 +18,18 @@ func task(id int, w *sync.WaitGroup) {
 func main() {
 	var wg sync.WaitGroup
 
+	// for i := 0; i <= 10; i++ {
+ 
+	// 	wg.Add(1)
+	// 	go task(i, &wg)
+	// }
+
 	for i := 0; i <= 10; i++ {
-		wg.Add(1)
+		wg.Add(2)
+		go task(i, &wg)
 		go task(i, &wg)
 	}
 
+	// time.Sleep(time.Millisecond * 1) // wait for 100 milliseconds to let all goroutines finish
 	wg.Wait()
 }
